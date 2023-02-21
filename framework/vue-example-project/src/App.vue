9af1ad293772603b69ce840beff18fe9a4596885
BNTang <303158131@qq.com>
2023-02-21 21:10:35 +0800

    add 功能

---
+++ framework/vue-example-project/src/App.vue
@@ -0,0 +1,59 @@
+<template>
+    <div id="app">
+        <div>
+            <el-row>
+                <el-button>默认按钮</el-button>
+                <el-button type="primary">主要按钮</el-button>
+                <el-button type="success">成功按钮</el-button>
+                <el-button type="info">信息按钮</el-button>
+                <el-button type="warning">警告按钮</el-button>
+                <el-button type="danger">危险按钮</el-button>
+            </el-row>
+        </div>
+        <div style="width: 500px; height: 500px;">
+            <tableComp/>
+        </div>
+    </div>
+</template>
+
+<script>
+import tableComp from "@/components/tableComp.vue";
+export default {
+    name: 'App',
+    components: {
+        tableComp
+    },
+    data() {
+        return {
+            tableData: [{
+                date: '2016-05-02',
+                name: '王小虎',
+                address: '上海市普陀区金沙江路 1518 弄'
+            }, {
+                date: '2016-05-04',
+                name: '王小虎',
+                address: '上海市普陀区金沙江路 1517 弄'
+            }, {
+                date: '2016-05-01',
+                name: '王小虎',
+                address: '上海市普陀区金沙江路 1519 弄'
+            }, {
+                date: '2016-05-03',
+                name: '王小虎',
+                address: '上海市普陀区金沙江路 1516 弄'
+            }]
+        }
+    }
+}
+</script>
+
+<style>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
