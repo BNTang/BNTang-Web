1416513fe72ebd7eefa7fda40e9b27cf26c9e2c6
BNTang <303158131@qq.com>
2023-02-21 21:23:25 +0800

    add 功能

--- framework/vue-example-project/src/main.js
+++ framework/vue-example-project/src/main.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
-import ElementUI from 'element-ui';
+import {Button, Table, TableColumn, Row} from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.config.productionTip = false
-Vue.use(ElementUI);
+Vue.use(Button);
+Vue.use(Table);
+Vue.use(TableColumn);
+Vue.use(Row);
 
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
 }).$mount('#app')
