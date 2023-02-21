494c8c5919049a0c2b7ef037b811cb77d845cd63
BNTang <303158131@qq.com>
2023-02-21 23:41:14 +0800

    add 功能

--- framework/vue-example-project/src/App.vue
+++ framework/vue-example-project/src/App.vue
@@ -4,7 +4,7 @@
             <el-row>
                 <el-button>默认按钮</el-button>
                 <el-button type="primary">主要按钮</el-button>
-                <el-button type="success">成功按钮</el-button>
+                <el-button type="success" @click="btnClick()">成功按钮</el-button>
                 <el-button type="info">信息按钮</el-button>
                 <el-button type="warning">警告按钮</el-button>
                 <el-button type="danger">危险按钮</el-button>
@@ -32,6 +32,14 @@ export default {
         return {
             value1: true,
             value2: true
+        }
+    },
+    methods: {
+        btnClick() {
+            this.$message({
+                message: '恭喜你，这是一条成功消息',
+                type: 'success'
+            });
         }
     }
 }
--- framework/vue-example-project/src/main.js
+++ framework/vue-example-project/src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
-import {Button, Table, TableColumn, Row, Switch} from 'element-ui';
+import {Button, Table, TableColumn, Row, Switch, Message} from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.config.productionTip = false
@@ -9,6 +9,8 @@ Vue.use(Table);
 Vue.use(TableColumn);
 Vue.use(Row);
 Vue.use(Switch);
+// 全局注册message
+Vue.prototype.$message = Message;
 
 new Vue({
     render: h => h(App),
