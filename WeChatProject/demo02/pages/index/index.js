1be70dba354deb89ca27e4f4655db61607f27191
BNTang <303158131@qq.com>
2023-04-13 23:46:33 +0800

    add code file

--- WeChatProject/demo02/pages/index/index.js
+++ WeChatProject/demo02/pages/index/index.js
@@ -1,13 +1,31 @@
 // pages/index/index.js
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-
+    objectArray: [
+      {id: 5, unique: 'unique_5'},
+      {id: 4, unique: 'unique_4'},
+      {id: 3, unique: 'unique_3'},
+      {id: 2, unique: 'unique_2'},
+      {id: 1, unique: 'unique_1'},
+      {id: 0, unique: 'unique_0'},
+    ],
+    chs: ['a', 'b', 'c'],
+    person: {
+      name: 'BNTang',
+      age: 21,
+      gender: 'male',
+      score: 100
+    }
   },
-
+  _myClickHandle: function(){
+    console.log("加之前：" + this.data.chs)
+    this.data.chs.unshift('d');
+    this.setData(this.data);
+    console.log("加之后：" + this.data.chs)
+  },
   /**
    * 生命周期函数--监听页面加载
    */
