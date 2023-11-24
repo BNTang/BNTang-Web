80e92198a0c30f900e5c22d25564b10a8287cad6
tangyihao <18819522037@163.com>
2023-11-24 17:13:43 +0800

    添加瀑布流

--- 功能/照片墙/index.js
+++ 功能/照片墙/index.js
@@ -1,12 +1,27 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var grid = document.querySelector('.grid');
-    var masonry = new Masonry(grid, {
-        itemSelector: '.grid-item',
-        columnWidth: '.grid-item',
+/**
+ * Created by BNTang on 2023-11-24 17:03:19
+ */
+const ITEM_CLASS_NAME = '.grid-item';
+const DOMContentLoaded_EVENT = 'DOMContentLoaded';
+const PROGRESS_EVENT = 'progress';
+
+function createImages() {
+}
+
+document.addEventListener(DOMContentLoaded_EVENT, function () {
+    // 获取布局元素
+    const grid = document.querySelector('.grid');
+
+    // 初始化布局
+    const masonry = new Masonry(grid, {
+        itemSelector: ITEM_CLASS_NAME,
+        columnWidth: ITEM_CLASS_NAME,
+        // percentPosition 设置为 true，将使用 columnWidth 作为百分比宽度：
         percentPosition: true
     });
 
-    imagesLoaded(grid).on('progress', function () {
+    // 图片加载完成后，重新布局
+    imagesLoaded(grid).on(PROGRESS_EVENT, function () {
         // layout Masonry after each image loads
         masonry.layout();
     });
