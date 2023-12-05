94d21df540e78c5bfe2364c24421acfeb9ad2305
BNTang <303158131@qq.com>
2023-12-06 00:17:33 +0800

    add new project

---
+++ WeChatProject/demo13/miniprogram/envList.js
@@ -0,0 +1,6 @@
+const envList = [{"envId":"cloud1-5gnw7kej2c68cd30","alias":"cloud1"}]
+const isMac = false
+module.exports = {
+    envList,
+    isMac
+}
---
+++ WeChatProject/demo13/uploadCloudFunction.bat
@@ -0,0 +1,1 @@
+"E:\apps\微信web开发者工具\cli.bat" cloud functions deploy --e cloud1-5gnw7kej2c68cd30 --n quickstartFunctions --r --project "D:\IdeaPro\BNTang-Web\WeChatProject\demo13" --report_first --report
