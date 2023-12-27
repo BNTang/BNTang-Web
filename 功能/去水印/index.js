86bc50619e07d95b318b401bd1fdf1fe17ce9934
BNTang <303158131@qq.com>
2023-12-27 23:56:51 +0800

    add files

--- 功能/去水印/index.js
+++ 功能/去水印/index.js
@@ -2,7 +2,7 @@
 // @name         去除怪兽图的水印
 // @namespace    http://tampermonkey.net/
 // @version      1.0
-// @description  try to take over the world!
+// @description  去除指定网页中的图片水印
 // @author       BNTang
 // @match        https://cloud.tencent.com/developer/column/94230
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=tencent.com
@@ -13,13 +13,26 @@
 (function() {
     'use strict';
 
-    // Your code here...
-    // 从整个页面中获取 class 包含 water 的元素，并且从页面进行移除
-    // 获取包含特定类名的所有元素
-    const waterElements = document.querySelectorAll('.image-watermark');
+    // 从页面中移除包含特定类名的元素
+    const removeWatermark = () => {
+        const waterElements = document.querySelectorAll('.image-watermark');
+        waterElements.forEach(element => {
+            element.remove();
+        });
+    };
 
-    // 从页面中移除这些元素
-    waterElements.forEach(element => {
-        element.remove();
+    // 由于部分内容可能是动态加载的，使用 MutationObserver 来观察 DOM 变化
+    const observer = new MutationObserver(mutations => {
+        mutations.forEach(mutation => {
+            if (mutation.addedNodes.length) {
+                removeWatermark();
+            }
+        });
     });
+
+    // 开始观察整个文档的变化
+    observer.observe(document.body, { childList: true, subtree: true });
+
+    // 首次运行以处理当前页面
+    removeWatermark();
 })();
