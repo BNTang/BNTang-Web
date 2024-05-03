26f097c0ffce76fd1b48b3587c0ac1d67a4ca7a4
BNTang <303158131@qq.com>
2024-05-03 23:41:24 +0800

    add code

--- Vue/37-NueCli-添加自定义指令/bin/index.js
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/env node
-
-const { program } = require('commander');
-const { version } = require('./const');
-
-program.version(version).parse(process.argv);
--- Vue/NueCli/bin/index.js
+++ Vue/NueCli/bin/index.js
@@ -1,19 +1,45 @@
 #! /usr/bin/env node
 
 const { program } = require('commander');
+const path = require('path');
 const { version } = require('./const');
-
-program
-  .command('create')
-  .alias('c')
-  .description('create a new project powered by nue-cli-service')
-  .action(() => {
-    console.log('创建一个 Nue 项目');
-  });
+const commandMap = {
+    create: {
+        alias: 'c',
+        description: 'create a new project powered by vue-cli-service',
+        example: 'nue-cli create <app-name>',
+    },
+    add: {
+        alias: 'a',
+        description: 'install a plugin and invoke its generator in an already created project',
+        example: 'nue-cli add [options] <plugin> [pluginOptions]',
+    },
+    '*': {
+        alias: '',
+        description: 'command not found',
+        example: '',
+    },
+};
+Reflect.ownKeys(commandMap).forEach((key) => {
+    const value = commandMap[key];
+    program
+        .command(key)
+        .alias(value.alias)
+        .description(value.description)
+        .action(() => {
+            if (key === '*') {
+                console.log(value.description);
+            } else {
+                require(path.resolve(__dirname, key))(...process.argv.slice(3));
+            }
+        });
+});
 program.on('--help', () => {
-  console.log('');
-  console.log('Examples:');
-  console.log('  nue create <app-name>  ');
+    console.log('Examples:');
+    Reflect.ownKeys(commandMap).forEach((key) => {
+        const value = commandMap[key];
+        console.log(`  ${value.example}`);
+    });
 });
 program
   .version(version)
