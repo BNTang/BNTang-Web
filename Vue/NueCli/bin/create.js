6d90f8ffb34f6f8c2b9a37d0100cc4afb7e00d0a
BNTang <303158131@qq.com>
2024-05-05 15:42:09 +0800

    『手撕Vue-CLI』拉取模板名称

--- Vue/NueCli/bin/create.js
+++ Vue/NueCli/bin/create.js
@@ -1,3 +1,18 @@
-module.exports = (projectName) => {
-    console.log('create', projectName);
+// 导入 axios
+const axios = require('axios');
+// 导入 ora，用于显示加载中
+const ora = require('ora');
+
+const fetchRepoList = async () => {
+    const spinner = ora('Loading template list...').start();
+    const { data } = await axios.get('https://api.github.com/orgs/neo-it6666/repos')
+    spinner.succeed('Template list loaded successfully');
+    return data;
+}
+
+module.exports = async (projectName) => {
+    // 获取模板列表
+    const fetchRepoListData = await fetchRepoList();
+    const templateNames =  fetchRepoListData.map((item) => item.name);
+    console.log(templateNames);
 }
