efcc07715e797285f7ae3f0a448ce30f5b7c799a
BNTang <303158131@qq.com>
2024-05-12 23:40:32 +0800

    add code

--- Vue/NueCli/bin/const.js
+++ Vue/NueCli/bin/const.js
@@ -1,5 +1,9 @@
 const { version } = require('../package.json');
 
+const currentPlatformKey = process.platform === 'win32' ? 'USERPROFILE' : 'HOME';
+const downloadDirPath = `${process.env[currentPlatformKey]}\\.nue-template`;
+
 module.exports = {
   version,
+  downloadDirPath
 };
