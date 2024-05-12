b38b14cc8ac32a5359916e53b5d493c030f3f404
BNTang <303158131@qq.com>
2024-05-13 00:48:31 +0800

    『手撕Vue-CLI』下载指定模板

--- Vue/NueCli/bin/create.js
+++ Vue/NueCli/bin/create.js
@@ -1,16 +1,37 @@
+// 导入 util 模块
+const {promisify} = require('util');
 // 导入 axios
 const axios = require('axios');
 // 导入 ora，用于显示加载中
 const ora = require('ora');
 // 导入 inquirer，用于命令行交互
 const inquirer = require('inquirer');
+// 导入 download-git-repo，用于下载模板
+const downloadGitRepo = promisify(require('download-git-repo'));
+// 导入 downloadDirPath，用于获取下载路径
+const {downloadDirPath} = require('./const');
+
+const downloadTemplate = async (templateName, version) => {
+    // 组织机构的名称/模板名称#版本号
+    // 1.拼接模板在github上的地址
+    let url = `neo-it6666/${templateName}`;
+
+    if (version) {
+        url += `#${version}`;
+    }
+
+    // 2.拼接存储下载好的模板的路径
+    const downloadPath = `${downloadDirPath}\\${templateName}`;
+    await downloadGitRepo(url, downloadPath);
+    return downloadPath;
+}
 
 const fetchRepoList = async () => {
     const {data} = await axios.get('https://api.github.com/orgs/neo-it6666/repos')
     return data;
 }
 
-const waitLoading =  ( message, fn) => async (...args) => {
+const waitLoading = (message, fn) => async (...args) => {
     const spinner = ora(message).start();
     const result = await fn(...args);
     spinner.succeed(`${message} successfully`);
@@ -42,10 +63,13 @@ module.exports = async (projectName) => {
     const fetchTemplateTags = await waitLoading('downloading template tags...', getTemplateTags)(template);
     const tags = fetchTemplateTags.map((item) => item.name);
 
-    const { version } = await inquirer.prompt({
+    const {version} = await inquirer.prompt({
         name: 'version',
         type: 'list',
         message: 'Please select the version number',
         choices: tags
     })
+
+    const destPath = waitLoading('downloading template...', downloadTemplate)(template, version)
+    console.log(destPath)
 }
