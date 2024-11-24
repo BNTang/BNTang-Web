1fec7cac6341d9b4c9f7960f56431a731a559b54
BNTang <303158131@qq.com>
2024-11-24 14:18:25 +0800

    feat(ES6): 添加模块化配置和测试
    - 新增 ES6 模块配置文件 index.js
    - 添加测试文件 test.js以验证模块导入
    - 新建 Node.js 路径解析示例
    - 增加测试用的 iconfont 文件和 HTML 文件

---
+++ ES6/Module/index.js
@@ -0,0 +1,22 @@
+const config = {
+    menuItemsConfig: [
+        {
+            icon: 'https://foruda.gitee.com/images/1729779202924786241/0910c0aa_8838873.png',
+            label: '短视频去水印',
+            url: '/pages/video/remove-watermark/index'
+        },
+        {
+            icon: 'https://foruda.gitee.com/images/1729778987074095580/2d7144c5_8838873.png',
+            label: '疯狂星期四',
+            url: '/pages/kfc/index'
+        },
+        // {
+        //   icon: 'https://foruda.gitee.com/images/1729779156097683071/9badd7ae_8838873.png',
+        //   label: '图集解析',
+        //   url: '/pages/graphical/index'
+        // }
+    ]
+}
+export {
+    config
+}
